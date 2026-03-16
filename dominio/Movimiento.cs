6a1444b5f138bfdf3f557bde43e39257b8293696
MerinaPinti <marinacelestepinti2@gmail.com>
2026-03-15 22:11:49 -0300

    Se creo clase auxiliar para mostrar grid de movimientos de dinero

---
+++ dominio/Movimiento.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Movimiento
+    {
+        //Se utiliza como clase intermedia para mostrar el grid principal 
+        public DateTime Fecha { get; set; }
+        public string Descripcion { get; set; }
+        public string Categoria { get; set; }
+        public string Tipo { get; set; }   // Ingreso o Gasto
+        public decimal Monto { get; set; }
+        public string Estado { get; set; }
+
+        public string MontoMostrado
+        {
+            get
+            {
+                if (Tipo == "Gasto")
+                    return "- $ " + Monto.ToString("N2");
+                else
+                    return "+ $ " + Monto.ToString("N2");
+            }
+        }
+    }
+}
