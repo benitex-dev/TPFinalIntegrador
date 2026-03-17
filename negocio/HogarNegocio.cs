3b5dd7436c3cfda258b72df742d06fe78732b4d0
Ailinmino <daianaailin.40@gmail.com>
2026-03-17 19:47:28 -0300

    HOGARNEGOCIO
    ALta

--- negocio/HogarNegocio.cs
+++ negocio/HogarNegocio.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using dominio;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,7 +10,39 @@ namespace negocio
     class HogarNegocio
     {
         //ALTA
+        public void AgregarHogar(Hogar nuevo)
+        {
+            AccesoDatos datos = new AccesoDatos();
 
+            try
+            {
+                if (nuevo == null)
+                    throw new Exception("El hogar no puede ser nulo.");
+
+                if (string.IsNullOrWhiteSpace(nuevo.Nombre))
+                    throw new Exception("El nombre del hogar es obligatorio.");
+
+                if (nuevo.Usuario == null || nuevo.Usuario.IdUsuario <= 0)
+                    throw new Exception("El hogar debe estar asociado a un usuario.");
+
+                datos.setConsulta("INSERT INTO HOGAR (Nombre, IdUsuario, Estado) " +
+                                  "VALUES (@nombre, @idUsuario, @estado)");
+
+                datos.setParametro("@nombre", nuevo.Nombre.Trim());
+                datos.setParametro("@idUsuario", nuevo.Usuario.IdUsuario);
+                datos.setParametro("@estado", nuevo.Estado);
+
+                datos.ejecutarAccion();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                datos.cerrarConexion();
+            }
+        }
         //BAJA
 
         //MODIFICAR
