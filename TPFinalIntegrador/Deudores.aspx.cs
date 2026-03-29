10b1c67fb8999972066af61b45afd97b15fee6b3
benitex-dev <benitezjorgedev@gmail.com>
2026-03-29 14:52:00 -0300

    se agrega pantalla para gestión de deudores

---
+++ TPFinalIntegrador/Deudores.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TPFinalIntegrador
+{
+    public partial class Deudores : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAgregar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void gvDeudas_RowEditing(object sender, GridViewEditEventArgs e)
+        {
+
+        }
+
+        protected void gvDeudas_RowUpdating(object sender, GridViewUpdateEventArgs e)
+        {
+
+        }
+
+        protected void gvDeudas_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+        {
+
+        }
+
+        protected void gvDeudas_RowDeleting(object sender, GridViewDeleteEventArgs e)
+        {
+
+        }
+
+        protected void btnAgregar_Click1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
